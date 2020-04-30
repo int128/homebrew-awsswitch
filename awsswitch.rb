e767b41fd03bc39c795fb8e6b65a28950a1116f4
Hidetake Iwata <int128@gmail.com>
2020-04-30 22:41:51 +0900

    Bump the version to v0.2.0 (#2)

--- awsswitch.rb
+++ awsswitch.rb
@@ -1,9 +1,9 @@
 class Awsswitch < Formula
   desc "Export the credentials variables to switch a role with MFA"
   homepage "https://github.com/int128/awsswitch"
-  url "https://github.com/int128/awsswitch/releases/download/v0.1.0/awsswitch_darwin_amd64.zip"
-  version "v0.1.0"
-  sha256 "5ec7725339911b08d2a9a9e83d2a8c861bd1593f54aebf02091dabbb4fecde04"
+  url "https://github.com/int128/awsswitch/releases/download/v0.2.0/awsswitch_darwin_amd64.zip"
+  version "v0.2.0"
+  sha256 "a9e9b7121d1412c283c1b6ecbf9fa93c2635cffe289d2e7655882fb5d7e2dc83"
   def install
     bin.install "awsswitch"
   end
